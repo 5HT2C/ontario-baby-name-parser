f030110511ca03d3803e5d732b289fdb34b43414
5HT2 <git@frogg.ie>
2024-05-02 19:51:07 -0400

    impr: Start implementing regex matching

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -18,11 +19,14 @@ var (
 	csvF = flag.Bool("csv", false, "Use CSV output")
 	filF = flag.Bool("f", false, "Filter to results with sex 'F'")
 	filM = flag.Bool("m", false, "Filter to results with sex 'M'")
+	filR = flag.Bool("r", false, "Filter with regex matching")
 )
 
 func main() {
 	flag.Parse()
 
+	//
+	// Validate data and args
 	female, err := os.Open("data/ontario_top_baby_names_female_1917-2022_en_fr.csv")
 	if err != nil {
 		panic(err)
@@ -36,22 +40,47 @@ func main() {
 		flag.Usage()
 		return
 	}
+	//
 
-	upperName := strings.ToUpper(*name)
+	//
+	// Set vars
+	showF := *filF == true || (*filF == false && *filM == false)
+	showM := *filM == true || (*filF == false && *filM == false)
+
+	var filter *regexp.Regexp = nil
+	var validName = *name
+	//
+
+	// Check for valid regex
+	if f, err := regexp.Compile(fmt.Sprintf("(?i)%s", validName)); err != nil || *filF != true {
+		// don't use regex if it fails to compile
+		validName = strings.ToUpper(validName)
+
+		if err != nil && *csvF != true {
+			log.Printf("Failed to compile regex, reverting to string compare mode: %v\n", err)
+		}
+	} else {
+		filter = f
+		validName = filter.String()
+	}
+
 	if *csvF != true {
-		fmt.Printf("Parsing name: %s\n\n", upperName)
+		log.Printf(
+			"\nMATCHING NAME:\t%s\nUSE REGEX:\t%v\nSHOW MALE:\t%v\nSHOW FEMALE:\t%v\n\n",
+			validName, filter != nil, showM, showF,
+		)
 	}
 
-	if *filF == true || (*filF == false && *filM == false) {
-		read(female, upperName, "F")
+	if showF {
+		read(female, "F", validName, filter)
 	}
 
-	if *filM == true || (*filF == false && *filM == false) {
-		read(male, upperName, "M")
+	if showM {
+		read(male, "M", validName, filter)
 	}
 }
 
-func read(file *os.File, name string, sex string) {
+func read(file *os.File, sex string, name string, filter *regexp.Regexp) {
 	r := csv.NewReader(file)
 	currentYear := time.Now().Year()
 
@@ -71,12 +100,21 @@ func read(file *os.File, name string, sex string) {
 		if *last > 0 && int64(currentYear-year) > *last {
 			continue
 		}
-		if record[1] == name {
+
+		if matchName(record[1], name, filter) {
 			if *csvF != true {
-				fmt.Printf("[%s] Year %s (%s)\n", sex, record[0], record[2])
+				fmt.Printf("[%s] %s (%s)\n", sex, record[0], record[2])
 			} else {
-				fmt.Printf("%s,%s,%s\n", sex, record[0], record[2])
+				fmt.Printf("%s,%s,%s,%s\n", sex, record[0], record[1], record[2])
 			}
 		}
+	}
+}
+
+func matchName(record string, name string, filter *regexp.Regexp) bool {
+	if filter == nil {
+		return record == name
+	} else {
+		return filter.MatchString(record)
 	}
 }
